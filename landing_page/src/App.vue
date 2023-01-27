e6942e0accd76fcde16967cf30635d5a34ef9e35
Nickolas Ferreti <88321056+nickferreti@users.noreply.github.com>
2023-01-27 20:56:50 -0300

    Adicionando primeiros componentes

--- landing_page/src/App.vue
+++ landing_page/src/App.vue
@@ -1,32 +1,100 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer 
+    v-model="drawer" 
+    dark
+    app
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Design principle
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Seu design aqui
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in Designs"
+          :key="item.title"
+          link
+          :to="item.to"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item
+          v-for="item in Sobre"
+          :key="item.title"
+          link
+          :to="item.to"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>
+        Designs
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+      <v-container>
+        <v-row>
+          <v-col
+            v-for="n in 24"
+            :key="n"
+            cols="4"
+          >
+            <v-card height="200">
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+  export default {
+    data: () => ({ 
+      drawer: null,
+      Designs: [
+        { title: 'Designs', icon: 'mdi-view-dashboard', to:"/Designs" },
+      ],
+      Sobre: [
+        { title: 'Sobre', icon: 'mdi-image', to:"/Sobre" },
+      ]
+    }),
   }
+</script>
+
+<style>
+.hover:hover {
+  color: darkgray;
 }
 </style>
---
+++ landing_page/src/views/Sobre.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="Sobre">
+    <h1>Isso é uma pagina para informações adicionais</h1>
+  </div>
+</template>
